d1d52ac5d00f1c8fcc2d38ea23b2a92a304dcb30
ValentinDesaunay <90374959+ValentinDesaunay@users.noreply.github.com>
2021-10-15 10:17:10 +0200

    Add files via upload

---
+++ TP_Socket_Client/nbproject/Makefile-variables.mk
@@ -0,0 +1,35 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Debug configuration
+CND_PLATFORM_Debug=GNU-Linux
+CND_ARTIFACT_DIR_Debug=dist/Debug/GNU-Linux
+CND_ARTIFACT_NAME_Debug=tp_socket_client
+CND_ARTIFACT_PATH_Debug=dist/Debug/GNU-Linux/tp_socket_client
+CND_PACKAGE_DIR_Debug=dist/Debug/GNU-Linux/package
+CND_PACKAGE_NAME_Debug=tpsocketclient.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/GNU-Linux/package/tpsocketclient.tar
+# Release configuration
+CND_PLATFORM_Release=GNU-Linux
+CND_ARTIFACT_DIR_Release=dist/Release/GNU-Linux
+CND_ARTIFACT_NAME_Release=tp_socket_client
+CND_ARTIFACT_PATH_Release=dist/Release/GNU-Linux/tp_socket_client
+CND_PACKAGE_DIR_Release=dist/Release/GNU-Linux/package
+CND_PACKAGE_NAME_Release=tpsocketclient.tar
+CND_PACKAGE_PATH_Release=dist/Release/GNU-Linux/package/tpsocketclient.tar
+#
+# include compiler specific variables
+#
+# dmake command
+ROOT:sh = test -f nbproject/private/Makefile-variables.mk || \
+	(mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk)
+#
+# gmake command
+.PHONY: $(shell test -f nbproject/private/Makefile-variables.mk || (mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk))
+#
+include nbproject/private/Makefile-variables.mk
